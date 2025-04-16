f4dbe0cf0ab186bd65af64ec82d74e2eb0fc61c4
bepoadewale <bepoadewale@yahoo.com>
2025-04-16 23:19:31 +0100

    c4

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "eks-deploy-html-website-aws-autoscaling-group"
+    bucket         = "eks-deploy-html-website"
     key            = "html-app/terraform.tfstate"
     region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
