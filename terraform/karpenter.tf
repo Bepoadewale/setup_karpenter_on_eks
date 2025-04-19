6615a4292662dba7c64f62f0df6b2adbff22faff
bepoadewale <bepoadewale@yahoo.com>
2025-04-19 22:25:31 +0100

    c34

---
+++ terraform/karpenter.tf
@@ -0,0 +1,93 @@
+# terraform/karpenter.tf
+
+# IAM role for Karpenter controller
+resource "aws_iam_role" "karpenter_controller" {
+  name = "KarpenterControllerRole-${aws_eks_cluster.my_eks.name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach managed policies required by Karpenter
+resource "aws_iam_role_policy_attachment" "karpenter_node" {
+  role       = aws_iam_role.karpenter_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_vpc" {
+  role       = aws_iam_role.karpenter_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_registry" {
+  role       = aws_iam_role.karpenter_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_spot" {
+  role       = aws_iam_role.karpenter_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole"
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_instance_profile" {
+  role       = aws_iam_role.karpenter_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_instance_profile" "karpenter_instance_profile" {
+  name = "KarpenterNodeInstanceProfile"
+  role = aws_iam_role.karpenter_controller.name
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "karpenter" {
+  name       = "karpenter"
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  namespace  = "karpenter"
+  create_namespace = true
+
+  set {
+    name  = "settings.clusterName"
+    value = aws_eks_cluster.my_eks.name
+  }
+
+  set {
+    name  = "settings.clusterEndpoint"
+    value = aws_eks_cluster.my_eks.endpoint
+  }
+
+  set {
+    name  = "settings.aws.defaultInstanceProfile"
+    value = aws_iam_instance_profile.karpenter_instance_profile.name
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\.amazonaws\.com/role-arn"
+    value = aws_iam_role.karpenter_controller.arn
+  }
+} 
+
+# output for later use if needed
+output "karpenter_role_arn" {
+  value = aws_iam_role.karpenter_controller.arn
+} 
+
+output "karpenter_instance_profile" {
+  value = aws_iam_instance_profile.karpenter_instance_profile.name
+}
